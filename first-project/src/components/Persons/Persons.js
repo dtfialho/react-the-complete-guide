bf2e97b8ce857281034f2bbaa0384f75cc4a516c
Diego T. Fialho <diego.tfialho@gmail.com>
2018-07-27 10:52:37 -0300

    Converting stateless components to stateful

--- first-project/src/components/Persons/Persons.js
+++ first-project/src/components/Persons/Persons.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = ({ persons, click, change }) => persons.map((person, index) => {
-  return <Person
-    click={() => click(index)}
-    name={person.name}
-    age={person.age}
-    key={person.id}
-    changed={(event) => change(event, person.id)} />
-});
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return <Person
+        click={() => this.props.click(index)}
+        name={person.name}
+        age={person.age}
+        key={person.id}
+        changed={(event) => this.props.change(event, person.id)} />;
+    });
+  }
+}
 
 export default Persons;
