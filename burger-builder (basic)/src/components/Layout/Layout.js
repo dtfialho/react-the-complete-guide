401cb32a4805ae2b2ea07900c2d9acd48d8be384
Diego T. Fialho <diego.tfialho@gmail.com>
2018-09-18 11:11:47 -0300

    Burger builder container started

--- burger-builder (basic)/src/components/Layout/Layout.js
+++ burger-builder (basic)/src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliar';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
     <div>
       Toolbar, SideDrawer, Backdrop
     </div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
