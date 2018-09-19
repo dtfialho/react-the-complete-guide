f3e776c53e65aa04b435dc216765c7184f36026f
Diego T. Fialho <diego.tfialho@gmail.com>
2018-09-19 10:10:55 -0300

    Removing ingredients safely

--- burger-builder (basic)/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder (basic)/src/components/Burger/BuildControls/BuildControls.js
@@ -15,8 +15,10 @@ const buildControls = (props) => (
     { controls.map(control => (
       <BuildControl
         added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
         key={control.label}
-        label={control.label} />
+        label={control.label}
+        disabled={props.disabled[control.type]} />
     )) }
   </div>
 );
