56ba0893d855abf70a0ee651f41f2ac8024b6826
Diego T. Fialho <diego.tfialho@gmail.com>
2018-09-20 14:28:17 -0300

    Menu toggle

--- burger-builder (basic)/src/components/Layout/Layout.js
+++ burger-builder (basic)/src/components/Layout/Layout.js
@@ -7,17 +7,24 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   }
 
   sideDrawerCloseHandler = () => {
     this.setState({showSideDrawer: false});
   }
 
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar
+          drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           close={this.sideDrawerCloseHandler} />
